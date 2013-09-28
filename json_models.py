37b49bc5af4fffbd433c3a3dc9349b9a5cf5bd1e
Nic Roland <nicroland9@gmail.com>
2013-09-28 12:12:04 +0100

    Added GameState and PlayerState.

--- json_models.py
+++ json_models.py
@@ -1,4 +1,35 @@
 
+class PlayerState(object):
+    def __init__(self, blob):
+        self.update(blob)
+
+    def update(self, blob):
+        self.name = blob['name']
+        self._id = blob['id']
+
+        if 'token' in blob:
+            self.token = blob['token']
+
+        if 'won' in blob:
+            self.won = blob['won']
+
+        if 'lost' in blob:
+            self.lost = blob['lost']
+
+class GameState(object):
+    def __init__(self, blob):
+        self.update(blob)
+
+    def update(self, blob):
+        self._id = blob['id']
+        self.status = blob['status']
+
+        if 'height' in blob:
+            self.height = blob['height']
+
+        if 'width' in blob:
+            self.width = blob['width']
+
 class JSONResponseModels:
     """Contains objects for JSON requests"""
 
