c98b3344a8fb8ace0e88fce16beea45480e239e3
Thomas Geraghty <thethomaseffect@gmail.com>
2013-09-28 11:29:04 +0100

    Added api function signatures and added requirements.txt

---
+++ api.py
@@ -0,0 +1,7 @@
+import requests
+
+def get(request_string):
+    # TODO: Use request string with main url and return the json
+
+def post(request_string):
+    # TODO: Use request string with main url and return the json
