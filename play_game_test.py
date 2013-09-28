c7450802ddf6c545995293478779d915b8928427
Thomas Geraghty <thethomaseffect@gmail.com>
2013-09-28 17:14:14 +0100

    omg so gooooooood

--- play_game_test.py
+++ play_game_test.py
@@ -10,19 +10,23 @@ from statespace import StateSpace
 if __name__ == '__main__':
     # Create both players
     player1 = api.create_new_player("DROPdsf")
-    player2 = api.create_new_player("TESTasd")
+    #player2 = api.create_new_player("TESTasd")
     # Host starts game
+
+    #new_game = api.create_and_start_game(
+    #    token=player1["token"],player2_id=player2["id"])
+    player2_id = 435345
     new_game = api.create_and_start_game(
-        token=player1["token"],player2_id=player2["id"])
+        token=player1["token"],player2_id=player2_id)
     # Setup ships for both players
     player1_ship_poitions = api.set_ships_random(
         token=player1["token"], game_id=new_game["id"])
-    player2_ship_poitions = api.set_ships_random(
-        token=player2["token"], game_id=new_game["id"])
+    #player2_ship_poitions = api.set_ships_random(
+    #    token=player2["token"], game_id=new_game["id"])
     #print("Player 1 board: " + str(player1_ship_poitions))
     #print("Player 2 board: " + str(player2_ship_poitions))
-    while player2_ship_poitions["status"] != "fight":
-        sleep(0.1)
+    #while player1_ship_poitions["status"] != "fight":
+     #   sleep(0.1)
     # print("READY!")
     #statespace = StateSpace(x=player1_ship_poitions[""])
     #api.shoot(token=player1["token"], game_id=new_game["id"],
@@ -35,7 +39,7 @@ if __name__ == '__main__':
     for x in coords:
         api.shoot(token=player1["token"], game_id=new_game["id"],
             x=x[0],y=[1])
-        api.shoot(token=player2["token"], game_id=new_game["id"],
-            x=x[0],y=[1])
-        print("Shot")
+        #api.shoot(token=player2["token"], game_id=new_game["id"],
+        #    x=x[0],y=[1])
+        print("Shot: " + str(x))
     print("I'm done")
