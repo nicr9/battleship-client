8b98b8aa976a4cd8677f8f87d300e55f5af49ebf
Thomas Geraghty <thethomaseffect@gmail.com>
2013-09-28 14:37:17 +0100

    Added some api functions

--- api.py
+++ api.py
@@ -9,7 +9,15 @@ import requests
 API_ADDRESS = "http://battleship-env-1.elasticbeanstalk.com/"
 
 def create_new_player(player_name):
-    return requests.get(API_ADDRESS + "/player/new?=" + player_name
+    """ creates player and gets you a token"""
+    return requests.get(API_ADDRESS + "/player/new?=" + player_name).json()
+
+def create_and_start_game(token, player2_id):
+    """ sets game to ready and lets you set your ships"""
+    return requests.get(API_ADDRESS + token + "/game/new/" + player2_id).json()
+
+def join_game(token,game_id):
+    return requests.get(API_ADDRESS + token + "/game/" + game_id + "/join").json()
 
 def shoot(token, game_id, x, y):
     # TODO: Make sure game is in fight status
