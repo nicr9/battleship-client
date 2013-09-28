8dbe19b1fc76dcba5c2bbfa2580f97b6ae41a53d
Thomas Geraghty <thethomaseffect@gmail.com>
2013-09-28 15:01:43 +0100

    Added game player file with some todo comments

---
+++ play_game.py
@@ -0,0 +1,8 @@
+"""
+Actually plays the game.
+"""
+
+# TODO: Create Player
+# TODO: Create game with two people or join game
+# TODO: Being fighting using AI algos
+# TODO: Catch exception on shoot when game has finished
